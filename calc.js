1f4744937adb37d740060d434e0e2a4d7fca8850
notobd <notobd4x4@gmail.com>
2024-09-21 13:57:48 -0700

    minor change

--- calc.js
+++ calc.js
@@ -6,16 +6,16 @@ const addition = document.querySelector(".operations");
 const sum = document.querySelector(".total");
 let multadd, addMult;
 const operate = function () {
-  const addMult = Number(document.querySelector(".first").value);
-  const multAdd = Number(document.querySelector(".second").value);
+  const addMult = Number(firstNumber.value);
+  const multAdd = Number(secNumber.value);
   const multiply = addMult * multAdd;
   document.body.style.backgroundColor = "green";
   document.querySelector(".final").textContent = multiply;
 };
 document.querySelector(".operation").addEventListener("click", operate);
 const operate1 = () => {
-  const addPlus = Number(document.querySelector(".first").value);
-  const plusAdd = Number(document.querySelector(".second").value);
+  const addPlus = Number(firstNumber.value);
+  const plusAdd = Number(secNumber.value);
   const addition = addPlus + plusAdd;
   document.querySelector("body").style.backgroundColor = "purple";
   document.querySelector(".final").textContent = addition;
